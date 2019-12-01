0fd96ed7a960819827a823870c8c01a8794c7c7d
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-01 11:37:12 +0900

    [add]good

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,7 +3,7 @@ class PostImagesController < ApplicationController
 		@post_image = PostImage.new
     end
     def create
-    	@post_image = PostImage.new(post_image_params)
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
@@ -11,12 +11,15 @@ class PostImagesController < ApplicationController
     def index
     	@post_images = PostImage.all
     end
-    def show
-    	@post_image = PostImage.find(params[:id])
-    end
+
+def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
 end
+
 
 private
     def post_image_params
         params.require(:post_image).permit(:shop_name, :image, :caption)
     end
+end
