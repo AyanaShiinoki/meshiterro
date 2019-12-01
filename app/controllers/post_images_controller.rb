765c9ac8bb55dd102172b9d135d59065bcda9346
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-01 12:20:35 +0900

    [start]5-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -15,11 +15,16 @@ class PostImagesController < ApplicationController
     	@post_images = PostImage.page(params[:page]).reverse_order
     end
 
-def show
-    @post_image = PostImage.find(params[:id])
-    @post_comment = PostComment.new
-end
+    def show
+        @post_image = PostImage.find(params[:id])
+        @post_comment = PostComment.new
+    end
 
+    def destroy
+        @post_image = PostImage.find(params[:id])
+        @post_image.destroy
+        redirect_to post_images_path
+    end
 
 private
     def post_image_params
